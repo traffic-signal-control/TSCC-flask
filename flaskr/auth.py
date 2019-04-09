d504903a4c071908d4dc3e4998a942af4222589e
xyh97 <xiongyh@zju.edu.cn>
2019-04-09 12:13:43 +0800

    verification code

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -13,15 +13,56 @@ from wtforms import validators
 from flask_mail import Mail
 from flask_mail import Message
 from threading import Thread
+import random
+
+
+def generate_verification_code(length=4):
+    ''' 随机生成6位的验证码 '''
+    # 注意： 这里我们生成的是0-9A-Za-z的列表，当然你也可以指定这个list，这里很灵活
+    # 比如： code_list = ['P','y','t','h','o','n','T','a','b'] # PythonTab的字母
+    code_list = [] 
+    for i in range(10): # 0-9数字
+        code_list.append(str(i))
+    for i in range(65, 91): # 对应从“A”到“Z”的ASCII码
+        code_list.append(chr(i))
+    for i in range(97, 123): #对应从“a”到“z”的ASCII码
+        code_list.append(chr(i))
+    myslice = random.sample(code_list, length)  # 从list中随机获取6个元素，作为一个片断返回
+    verification_code = ''.join(myslice) # list to string
+    return verification_code
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 # 异步发送邮件
-def async_send_mail(app, msg, mail):
-    # 必须在程序上下文中才能发送邮件，新建的线程没有，因此需要手动创建
-    with app.app_context():
-        # 发送邮件
-        mail.send(msg)
+# def async_send_mail(app, msg, mail):
+#     # 必须在程序上下文中才能发送邮件，新建的线程没有，因此需要手动创建
+#     with app.app_context():
+#         # 发送邮件
+#         mail.send(msg)
+
+class async_send_mail(Thread):
+    def __init__(self, app, msg, mail):
+        super(async_send_mail, self).__init__()
+        self.app = app
+        self.msg = msg
+        self.mail = mail
+        self.exitcode = 0
+        self.exception = None
+
+    def run(self):
+        try:
+            self._run()
+        except Exception as e:
+            self.exitcode = 1
+            self.exception = e
+
+    def _run(self):
+        try:
+            with self.app.app_context():
+                self.mail.send(self.msg)
+        except Exception as e:
+            raise e
+
 
 
 def send_mail(to, subject, **kwargs):
@@ -29,12 +70,16 @@ def send_mail(to, subject, **kwargs):
     app = current_app._get_current_object()
     mail = Mail(app)
     # 创建邮件对象
-    msg = Message(subject, recipients=[to], sender=app.config['MAIL_USERNAME'])
-    msg.body = "dfsadasd"
-    msg.html = "<b>testsdsding</b>"
-    thr = Thread(target=async_send_mail, args=[app, msg, mail])
-    thr.start()
-    return thr
+    msg = Message("Verification Code", recipients=[to], sender=app.config['MAIL_USERNAME'])
+    # msg.body = "dfsadasd"
+    msg.html = "<b>{}</b>".format(subject)
+    # mail.send(msg)
+    # thr = Thread(target=async_send_mail, args=[app, msg, mail])
+    # thr.start()
+    t = async_send_mail(app, msg, mail)
+    t.start()
+    # t.join()
+    # return t.exitcode
 
 
 class UserForm(FlaskForm):
@@ -46,7 +91,7 @@ class UserForm(FlaskForm):
 
 
 class CodeForm(FlaskForm):
-
+    email = StringField('Email',[validators.Email("Please enter your email address.")])
     validate_code = StringField('Code', [validators.DataRequired("Please enter your validation code")])
     submit2 = SubmitField('Register')
 
@@ -77,18 +122,37 @@ def register():
             username = form.name.data
             password = form.password.data
             email = form.email.data
-            # print(username, file=sys.stderr)
-            # print(password, file=sys.stderr)
-            # print(email, file=sys.stderr)
-            send_mail(email, "aaaaaaaaaaa")
-            db.execute(
-                'INSERT INTO user (username, password, email, code) VALUES (?, ?, ?, ?)',
-                (username, generate_password_hash(password), email, 'xxx')
-            )
-            db.commit()
-            session['name'] = form.name.data
-            flash('注册成功，请移步至邮箱查看验证码激活')
-            return redirect(url_for('auth.activate'))
+            error = None
+            if not username:
+                error = 'Username is required.'
+            elif not password:
+                error = 'Password is required.'
+            elif not email:
+                error = 'Email is required.'
+            elif db.execute(
+                'SELECT id FROM user WHERE username = ?', (username,)
+            ).fetchone() is not None:
+                error = 'User {} is already registered.'.format(username)
+
+            else:
+                verification_code = generate_verification_code()
+                try:
+                    send_mail(email, verification_code)
+                except:
+                    error = "Failed to send email, please try again."
+                    flash(error)
+                    return render_template('auth/register.html', form=form)
+
+                db.execute(
+                    'INSERT INTO user (username, password, email, code) VALUES (?, ?, ?, ?)',
+                    (username, generate_password_hash(password), email, verification_code)
+                )
+                db.commit()
+                # session['name'] = form.name.data
+                flash('注册成功，请移步至邮箱查看验证码激活')
+                return redirect(url_for('auth.activate'))
+
+            flash(error)
         # if form2.submit2.data and form2.validate_on_submit():
 
         #     # print(form2.email.data, file=sys.stderr)
@@ -105,16 +169,21 @@ def activate():
     if request.method == 'POST':
         if form.submit2.data and form.validate_on_submit():
             db = get_db()
-            username = session['name']
+            # username = session['name']
+            email = form.email.data
             code = form.validate_code.data
-            print(username, file=sys.stderr)
+            # print(username, file=sys.stderr)
             print(code, file=sys.stderr)
             user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
+            'SELECT * FROM user WHERE email = ?', (email,)
         ).fetchone()
             error = None
+            if not user["code"] == code:
+                error = 'Incorrect verification code.'
             if error is None:
                 return redirect(url_for('auth.login'))
+
+            flash(error)
 
     return render_template('auth/activate.html', form=form)
 
