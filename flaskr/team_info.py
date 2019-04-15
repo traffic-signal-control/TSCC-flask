870d5afb3f755a89cba446c0242e377f6da1570b
zxsRambo <1690064956@qq.com>
2019-04-15 19:17:12 +0800

    add uploaded file validation

--- flaskr/team_info.py
+++ flaskr/team_info.py
@@ -2,6 +2,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, send_from_directory
 )
 from werkzeug.exceptions import abort
+from werkzeug import secure_filename
 
 from wtforms import StringField, SubmitField, PasswordField, SelectField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
@@ -12,6 +13,7 @@ from flaskr import config
 import sys
 import os
 import time
+import pandas as pd
 
 bp = Blueprint('team_info', __name__, url_prefix='/team_info')
 
@@ -57,6 +59,83 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+def check_upload_file(planFile, num_step):
+    flag = True
+    error_info = ''
+    try:
+        plan = pd.read_csv(planFile, sep='\t', header=0, dtype=int)
+    except:
+        flag = False
+        error_info = 'The format of signal plan is not valid and cannot be read by pd.read_csv!'
+        print(error_info)
+        return flag
+
+    intersection_id = plan.columns[0]
+    if intersection_id != 'intersection_1_1':
+        flag = False
+        error_info = 'The header intersection_id is wrong (for example: intersection_1_1)!'
+        print(error_info)
+        return flag
+
+    phases = plan.values
+    current_phase = phases[0][0]
+
+    if len(phases) < num_step:
+        flag = False
+        error_info = 'The time of signal plan is less than the default time!'
+        print(error_info)
+        return flag
+
+    if current_phase == 0:
+        yellow_time = 1
+    else:
+        yellow_time = 0
+
+    # get first green phase and check
+    last_green_phase = '*'
+    for num_phase in range(1, len(phases)):
+
+        next_phase = phases[num_phase][0]
+
+        # check phase itself
+        if next_phase not in [0, 1, 2, 3, 4, 5, 6, 7, 8]:
+            flag = False
+            error_info = 'Phase must be in [0, 1, 2, 3, 4, 5, 6, 7, 8]!'
+            break
+        if next_phase == '':
+            continue
+
+        # check changing phase
+        if next_phase != current_phase and next_phase != 0 and current_phase != 0:
+            flag = False
+            error_info = '5 seconds of yellow time must be inserted between two different phase!'
+            break
+
+        # check unchangeable phase
+        if next_phase != 0 and next_phase == last_green_phase:
+            flag = False
+            error_info = 'No yellow light is allowed between the same phase!'
+            break
+
+        # check yellow time
+        if next_phase != 0 and yellow_time != 0 and yellow_time != 5:
+            flag = False
+            error_info = 'Yellow time must be 5 seconds!'
+            break
+
+        # normal
+        if next_phase == 0:
+            yellow_time += 1
+            if current_phase != 0:
+                last_green_phase = current_phase
+        else:
+            yellow_time = 0
+        current_phase = next_phase
+
+    error_info = "In line %s: "%str(num_phase+1) + error_info
+    if not flag:
+        print(error_info)
+    return flag, error_info
 
 class UploadForm(Form):
     dataset = SelectField(label='Dataset', choices = [('default', '--'),
@@ -83,6 +162,9 @@ def create():
 
             print(dataset, file=sys.stderr)
             print(file.filename)
+
+            num_step = 3600
+
             dataset_name = None
             if dataset == 'default':
                 error = 'Please select one dataset'
@@ -94,10 +176,15 @@ def create():
             _time = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime(time.time()))
             filename = None
             if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
                 filename = "signal_plan-"+str(g.user['id']) + "-%s"%_time  + "-%s.txt"%dataset_name
                 if not os.path.exists(UPLOAD_FOLDER):
                     os.makedirs(UPLOAD_FOLDER)
                 file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+                flag, error_info = check_upload_file(os.path.join(UPLOAD_FOLDER, filename), num_step)
+                if not flag:
+                    error = 'File uploaded is invalid.\n' + error_info
             else:
                 error = 'Signal plan is required and the file name must have a ".txt" extension'
 
